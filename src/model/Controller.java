d5cabd2379f071d593972b50ca568cad38aab465
Daniel Mejia <1107836426@u.icesi.edu.co>
2023-05-02 05:55:48 -0500

    Array

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,5 +1,19 @@
 package model;
+import java.util.*;
+
 
 public class Controller {
     
+    ArrayList<Books> books;
+
+    public Controller() {
+        books = new ArrayList<>();
+    }
+
+
+    public void registerBook(){
+
+        
+    }
+
 }
