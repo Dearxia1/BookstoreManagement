42d41776daaa28393b208b6f1127221c882af369
Daniel Mejia <1107836426@u.icesi.edu.co>
2023-05-08 07:34:17 -0500

    Cositas nuevas en registo de libros nada mas y usuario ya terminado

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -5,15 +5,79 @@ import java.util.*;
 public class Controller {
     
     ArrayList<Books> books;
+    ArrayList<Regular> RegularUser;
+    ArrayList<Premiun> PremiunUser;
 
     public Controller() {
+
         books = new ArrayList<>();
+        RegularUser = new ArrayList<>();
+        PremiunUser = new ArrayList<>();
     }
 
 
-    public void registerBook(){
+    public void registerRegular_user(
 
+        String name,
+         String id){
+
+        Regular regular_user = new Regular(
+            name,
+            id,
+            null,
+            null,
+            null,
+            null);
+
+        RegularUser.add(regular_user);
         
     }
+
+    public void registerPremiun_user(
+
+        String name,
+         String id){
+
+        Premiun premiun_user = new Premiun(
+            name,
+             id,null,
+              null,
+               null,
+               null);
+
+        PremiunUser.add(premiun_user);
+            
+        }
+
+
+        public void registerBook(
+
+            String name,
+             String numberPages,
+              String publicationDate,
+               int accumulatedPagesRead,
+                String hexadecimalIdentifier,
+                 String shortReview,
+                  Genre genre,
+                  String urlBookCover,
+                   Double salesValue,
+                    int soldCopies) {
+
+            Books book = new Books(
+
+                name,
+                numberPages,
+                publicationDate,
+                accumulatedPagesRead,
+                hexadecimalIdentifier,
+                shortReview,
+                genre,
+                urlBookCover,
+                salesValue,
+                soldCopies);
+
+            books.add(book);
+        
+        }
 
 }
--- src/ui/Executable.java
+++ src/ui/Executable.java
@@ -22,29 +22,69 @@ public class Executable {
         boolean exit = false;
         while (!exit) {
             System.out.println("====================================================================================================");
-            System.out.println("|                                         MENÚ PRINCIPAL                                          |");
+            System.out.println("|                                         MENÚ PRINCIPAL                                           |");
             System.out.println("====================================================================================================");
-            System.out.println(" Opciones:                                                                                          ");
-            System.out.println("        1. Registrar usuarios, regulares y premium                                                  ");
-            System.out.println("        2. Gestionar productos bibliográficos: registrar, modificar y borrar libros y revistas.     ");
-            System.out.print("\n\n");
+            System.out.println("| Opciones:                                                                                        |");
+            System.out.println("|                                                                                                  |");
+            System.out.println(String.format("| %-5s %-90s |", "1.", "Registrar usuarios, regulares y premium"));
+            System.out.println(String.format("| %-5s %-90s |", "2.", "Gestionar productos bibliográficos: registrar, modificar y borrar libros y revistas."));
+            System.out.println("|                                                                                                  |");
+            System.out.println("|                                                                                                  |");
+            System.out.println("====================================================================================================");
             System.out.print("Digite su opcion: ");
+            
             int option = reader.nextInt();
             reader.nextLine(); // Leer el salto de línea después del número
             System.out.print("\n\n\n");
-
             switch (option) {
-                case 1: 
+                case 1:
+                    System.out.println("+----------------------------------------+");
+                    System.out.println("|          REGISTRAR USUARIOS             |");
+                    System.out.println("+----------------------------------------+");
+                    System.out.println("| Elija el tipo de usuario que desea ser:|");
+                    System.out.println("| 1. Regular                             |");
+                    System.out.println("| 2. Premium                             |");
+                    System.out.println("| 3. Salir                               |");
+                    System.out.println("+----------------------------------------+");
+                    System.out.print("Ingrese una opción: ");
+            
+                    int option20 = reader.nextInt();
+                    reader.nextLine();
+                    System.out.print("\n\n");
+            
+                    switch(option20){
+                        case 1:
+                            registerRegular_user();
+                            System.out.println("Usuario registrado con exito...\n\n");
+                            break;
+            
+                        case 2:
+                            registerPremiun_user();
+                            System.out.println("Usuario registrado con exito...\n\n");
+                            break;
+            
+                        case 3:
+                            System.out.println("Volviendo al menú principal...\n\n");
+                            break;
+            
+                        default:
+                            System.out.println("Opción inválida.\n\n");
+                            break;
+                    }
                     break;
                 case 2:
                     int option2 = 0;
                     do {
                         System.out.println("====================================");
-                        System.out.println("Gestión de productos bibliográficos");
-                        System.out.println("==================================== \n");
-                        System.out.println("1. Gestionar libros");
-                        System.out.println("2. Gestionar revistas");
-                        System.out.println("3. Salir\n");
+                        System.out.println("|      Gestión de productos        |");
+                        System.out.println("|          bibliográficos          |");
+                        System.out.println("====================================");
+                        System.out.println("|                                  |");
+                        System.out.println("|  1. Gestionar libros             |");
+                        System.out.println("|  2. Gestionar revistas           |");
+                        System.out.println("|  3. Salir                        |");
+                        System.out.println("|                                  |");
+                        System.out.println("====================================");
                         System.out.print("Ingrese una opción: ");
                         option2 = reader.nextInt();
                         reader.nextLine(); // consumir el salto de línea
@@ -53,19 +93,56 @@ public class Executable {
                         int option4 = 0;
                         switch(option2){
                             case 1:
-                                System.out.println("==================");
-                                System.out.println("Gestión de libros");
-                                System.out.println("==================\n");
-                                System.out.println("1. Registrar libro");
-                                System.out.println("2. Modificar libro");
-                                System.out.println("3. Borrar libro");
-                                System.out.println("4. Volver al menú principal\n");
+                                System.out.println("===============================");
+                                System.out.println("|        Gestión de libros     |");
+                                System.out.println("===============================");
+                                System.out.println("|                             |");
+                                System.out.println("|  1. Registrar libro         |");
+                                System.out.println("|  2. Modificar libro         |");
+                                System.out.println("|  3. Borrar libro            |");
+                                System.out.println("|  4. Volver al menú principal |");
+                                System.out.println("|                             |");
+                                System.out.println("===============================");
                                 System.out.print("Ingrese una opción: ");
                                 option3 = reader.nextInt();
                                 reader.nextLine(); // consumir el salto de línea
                                 System.out.print("\n\n\n");
-                                
+                    
                                 switch(option3){
+                                    case 1:
+                                        registerBook();
+                                        break;
+                                    case 2:
+                                        break;
+                                    case 3:
+                                        break;
+                                    case 4:
+                                        System.out.println("Volviendo al menú principal...");
+                                        break;
+                                    default:
+                                        System.out.println("Opción inválida. Intente nuevamente.");
+                                        break;
+                                }
+                    
+                                break;
+                                    
+                            case 2:
+                                System.out.println("===============================");
+                                System.out.println("|       Gestión de revistas    |");
+                                System.out.println("===============================");
+                                System.out.println("|                             |");
+                                System.out.println("|  1. Registrar revista       |");
+                                System.out.println("|  2. Modificar revista       |");
+                                System.out.println("|  3. Borrar revista          |");
+                                System.out.println("|  4. Volver al menú principal |");
+                                System.out.println("|                             |");
+                                System.out.println("===============================");
+                                System.out.print("Ingrese una opción: ");
+                                option4 = reader.nextInt();
+                                reader.nextLine(); // consumir el salto de línea
+                                System.out.print("\n\n\n");
+                                
+                                switch(option4){
                                     case 1:
                                         break;
                                     case 2:
@@ -79,36 +156,6 @@ public class Executable {
                                         System.out.println("Opción inválida. Intente nuevamente.");
                                         break;
                                 }
-                                break;
-
-                                
-                            case 2:
-                            System.out.println("==================");
-                            System.out.println("Gestión de revistas");
-                            System.out.println("==================\n");
-                            System.out.println("1. Registrar revista");
-                            System.out.println("2. Modificar revista");
-                            System.out.println("3. Borrar revista");
-                            System.out.println("4. Volver al menú principal\n");
-                            System.out.print("Ingrese una opción: ");
-                            option4 = reader.nextInt();
-                            reader.nextLine(); // consumir el salto de línea
-                            System.out.print("\n\n\n");
-                            
-                            switch(option4){
-                                case 1:
-                                    break;
-                                case 2:
-                                    break;
-                                case 3:
-                                    break;
-                                case 4:
-                                    System.out.println("Volviendo al menú principal...");
-                                    break;
-                                default:
-                                    System.out.println("Opción inválida. Intente nuevamente.");
-                                    break;
-                            }
                                 break;
                             case 3:
                                 System.out.println("¡Hasta luego!");
@@ -132,8 +179,61 @@ public class Executable {
     }
 
 
-    public void registerBook(){
+    public void registerRegular_user(){
 
-        
+        System.out.print("  Digite su numero de cedula: ");
+        String id = reader.nextLine();
+        System.out.print("  Digite su nombre completo ");
+        String name = reader.nextLine();
+
+        controller.registerRegular_user(name, id);
+
     }
+
+    public void registerPremiun_user(){
+
+        System.out.print("  Digite su numero de cedula: ");
+        String id = reader.nextLine();
+        System.out.print("  Digite su nombre completo ");
+        String name = reader.nextLine();
+
+        controller.registerPremiun_user(name, id);
+
+    }
+
+    public void registerBook(){
+        System.out.print("Identificador: ");
+        String id = reader.nextLine();
+        
+        System.out.print("Nombre del libro: ");
+        String name = reader.nextLine();
+        
+        System.out.print("Número de páginas: ");
+        int numPages = reader.nextInt();
+        reader.nextLine(); 
+        
+        System.out.print("Reseña corta: ");
+        String review = reader.nextLine();
+        
+        System.out.print("Fecha de publicación (dd/mm/yyyy): ");
+        String publicationDate = reader.nextLine();
+        
+        System.out.print("Género: ");
+        String genre = reader.nextLine();
+        
+        System.out.print("URL de la portada: ");
+        String url = reader.nextLine();
+        
+        System.out.print("Valor de venta (en USD): ");
+        double saleValue = reader.nextDouble();
+        reader.nextLine(); 
+        
+        // Imprimir la información recolectada
+        System.out.println("Libro registrado con exito....");
+
+        controller.registerBook(name, genre, publicationDate, numPages, id, review, null, url, saleValue, numPages);
+
+    }
+
 }
+
